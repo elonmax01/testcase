f9143ff2bd679a82f96acf57e28e7587c5a52bfd
elonmax01 <maxim.bazhko44@gmail.com>
2024-05-31 09:24:24 +0300

    update on 31 May, install flake8

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import ImageField
 from django.urls import reverse
-import uuid
 
 # Create your models here.
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -13,9 +13,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
+    
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -29,6 +27,7 @@ class Author(models.Model):
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -38,7 +37,6 @@ class Post(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField("Category", blank=True, related_name="categories")
     tags = models.ManyToManyField("Tag", blank=True)
-
 
     def __str__(self):
         return self.title
@@ -64,9 +62,6 @@ class Post(models.Model):
         return is_next
 
 
-
-
-
 class Tag (models.Model):
     name = models.CharField(max_length=100)
     count = models.IntegerField(default=0)
@@ -78,7 +73,6 @@ class Tag (models.Model):
         return self.name
 
 
-
 class Card(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -86,8 +80,6 @@ class Card(models.Model):
     time = models.CharField(max_length=50, null=True, blank=True)
     thumb = ImageField(upload_to='media/')
     amount = models.IntegerField()
-
-
 
     icon_status = (('C', 'car'), ('F', 'foot'))
     gastronomy_status = (('y', 'yes'), ('n', 'no'))
@@ -98,11 +90,10 @@ class Card(models.Model):
     gastronomy = models.CharField(max_length=1, choices=gastronomy_status, blank=True, default='n', help_text='Отметка для гастрономических маршрутов')
     wine = models.CharField(max_length=1, choices=wine_status, blank=True, default='n', help_text='Будет ли что выпить?')
     minskin = models.CharField(max_length=1, choices=minskin_status, blank=True, default='y', help_text='Маршрут внутригородской или с выездом за город?')
-
-
-
+    
     def __str__(self):
         return self.title
+
 
 class Review(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
