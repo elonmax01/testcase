407029d2410533ff888905ffabf1462ce762dc89
elonmax01 <maxim.bazhko44@gmail.com>
2025-10-29 00:07:09 +0300

    add model of comments

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Author, Card, Review, Category, Post, Tag
+from catalog.models import Author, Card, Review, Category, Post, Tag, Comment
 
 # Register your models here.
 # admin.site.register(Book)
@@ -23,7 +23,12 @@ class TagAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     pass
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Comment, CommentAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -102,5 +102,18 @@ class Review(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} om {}'.format(self.name, self.post)
 
